bd591b69db72f0610daa710fd4507ec723050c67
Juan Pablo Rubianes <rubianes@adobe.com>
2018-05-05 20:59:06 -0400

    question2 complete

--- src/test/java/PageSelectors.java
+++ src/test/java/PageSelectors.java
@@ -1,2 +1,55 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class PageSelectors {
+
+    WebDriver driver;
+    By findMeetingButton = By.cssSelector(".find-a-meeting");
+    By searchCityZip = By.id("meetingSearch");
+    By location = By.cssSelector(".location__name");
+    By day = By.cssSelector(".hours-list-item-day");
+    By hour = By.cssSelector(".hours-list-item-hours");
+
+    public PageSelectors(WebDriver Argdriver){
+        this.driver = Argdriver;
+    }
+
+    public String validateTitle() {
+        return driver.getTitle();
+    }
+
+    public String clickFindMeeting() throws Exception {
+        driver.findElement(findMeetingButton).click();
+        Thread.sleep(2000);
+        return driver.getTitle();
+    }
+
+    public String searchByZipCode(String zipCode) {
+        driver.findElement(searchCityZip).sendKeys(zipCode,Keys.ENTER);
+        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+        List<WebElement> locations = driver.findElements(location);
+        return locations.get(0).getText();
+    }
+
+    public String validateLocation() {
+        List<WebElement> locations = driver.findElements(location);
+        locations.get(0).click();
+        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+        return driver.findElement(location).getText();
+    }
+
+    public void getHoursOfOperation(String value) {
+        List<WebElement> days = driver.findElements(day);
+        List<WebElement> hours = driver.findElements(hour);
+        for (int i = 0; i < days.size(); i++){
+            if(value.equalsIgnoreCase(days.get(i).getText())){
+                System.out.println(hours.get(i).getText());
+            }
+        }
+    }
 }
--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,20 +1,44 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.SimpleFormatter;
 
 public class TestCases {
 
     static WebDriver driver;
+    PageSelectors pageSelectors;
 
     @Test
     public void testCaseQuestion2() throws Exception {
 
+        //Set the remote web driver and launch the browser
         System.setProperty("webdriver.gecko.driver","geckodriver");
         driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
         driver.get("https://www.weightwatchers.com/us/");
+        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+        pageSelectors = new PageSelectors(driver);
 
+        //Validate titles from step 2 , 3 and 7
+        Assert.assertEquals("Weight Loss Program, Recipes & Help | Weight Watchers",pageSelectors.validateTitle());
+        Assert.assertEquals("Find A Meeting: Get Schedules & Times Near You | Weight Watchers",pageSelectors.clickFindMeeting());
+        String expectedLocation = pageSelectors.searchByZipCode("10011");
+        System.out.println(expectedLocation);
+        Assert.assertEquals(expectedLocation,pageSelectors.validateLocation());
+        pageSelectors.getHoursOfOperation(getCurrentDay());
+        driver.close();
+
+    }
+
+    private String getCurrentDay() {
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        return new SimpleDateFormat("EEEE",Locale.ENGLISH).format(date.getTime());
     }
 }
